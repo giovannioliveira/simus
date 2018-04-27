0388d5998cfb25783b4905ecbbd8284fcbd45150
Giovanni Oliveira <giovanni@ufrj.br>
2018-04-27 11:17:56 -0300

    finish questao2

--- sources/questao2.asm
+++ sources/questao2.asm
@@ -1,20 +1,27 @@
-ORG 150
+ORG 65280
+
+; valor a ser armazenado no endereco das vars
+V1H EQU 8
+V1L EQU 9
+V2H EQU 8
+V2L EQU 9
+
+; endereco das vars
+MV1H EQU 171
+MV1L EQU 18
+MV2H EQU 205
+MV2L EQU 69
 
 str11: STR "-"
 str12: STR "1"
 str2: STR "0"
 str3: STR "1"
 
-var1h: DS 1
-var1l: DS 1
-var2h: DS 1
-var2l: DS 1
-mvar1h: DS 1
-mvar1l: DS 1
-mvar2h: DS 1
-mvar2l: DS 1
-mreth: DS 1
-mretl: DS 1
+var1: DS 2
+var2: DS 2
+mvar1: DS 2
+mvar2: DS 2
+mret: DS 2
 aux1: DS 1
 aux2: DS 1
 aux3: DS 1
@@ -26,14 +33,15 @@ PREPARE:
     NOP
     NOP
     NOP
-    LDA #012h
+    LDA MV1L
     PUSH
-    LDA #0ABh
+    LDA MV1H
     PUSH
-    LDA #045h
+    LDA MV2L
     PUSH
-    LDA #0CDh
+    LDA MV2H
     PUSH
+
 
 SWITCH:
     OUT #3
@@ -42,8 +50,7 @@ SWITCH:
     JZ CASE1
     SUB #1
     JZ CASE2
-    SUB #1
-    JZ CASE3
+    JMP CASE3
 
 CASE1:
     LDA str11
@@ -63,25 +70,100 @@ CASE3:
     JMP END
 
 ROTINA:
+
     POP
-    STA mreth
+    STA mret+1
     POP
-    STA mretl
+    STA mret
     POP
-    STA mvar1h
+    STA mvar2+1
     POP
-    STA mvar1l
+    STA mvar2
     POP
-    STA mvar2h
+    STA mvar1+1
     POP
-    STA mvar2l
-    LDA mretl
+    STA mvar1
+    
+ 
+    LDA V1L
+    STA @mvar1
+    STA var1
+
+    LDA mvar1
+    ADD #1
+    STA mvar1
+
+    LDA V1H
+    STA @mvar1
+    STA var1+1
+
+    LDA mvar1
+    SUB #1
+    STA mvar1
+    
+    LDA V2L
+    STA @mvar2
+    STA var2
+
+    LDA mvar2
+    ADD #1
+    STA mvar2
+
+    LDA V2H
+    STA @mvar2
+    STA var2+1
+
+    LDA mvar2
+    SUB #1
+    STA mvar2
+
+    LDA mret
     PUSH
-    LDA mreth
+    LDA mret+1
     PUSH
+
+    LDA var1+1
+    AND #8
+    JZ POSA
+
+NEGA:
+    LDA var2+1
+    AND #8
+    JZ BMAIOR
+    JMP CMP
+
+POSA:
+    LDA var2+1
+    AND #8
+    JZ CMP
+    JMP AMAIOR
+
+CMP:
+    LDA var1+1
+    SUB var2+1
+    JN BMAIOR
+    JZ CMP2
+    JMP AMAIOR
+
+CMP2:
+    LDA var1
+    SUB var2
+    JN BMAIOR
+    JZ IGUAIS
+    JMP AMAIOR
+
+IGUAIS:
+    LDA #2
+    RET
+
+AMAIOR:
+    LDA #3
+    RET
+
+BMAIOR:
     LDA #1
     RET
-    
+
 END: 
     HLT
     
